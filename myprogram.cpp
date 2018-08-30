329354eeb326f92f4409dd2dbfc7c8c8ec5fadf5
Egil Sperstad K|ller <egilsk@pascal.uio.no>
2018-08-30 12:03:58 +0200

    Add cpp-file with armadillo

--- myprogram.cpp
+++ myprogram.cpp
@@ -1,34 +1,27 @@
-#include <cstdlib>
 #include <iostream>
-#include <cmath>
-#include <iomanip> 
-#include "time.h"   // Not the use time.h
+#include "armadillo"
+using namespace arma;
+using namespace std;
 
-using namespace std; // note use of namespace                                       
-int main (int argc, char* argv[])
-{
-  int i = atoi(argv[1]); 
-  double *a, *b, *c;
-  a = new double[i]; 
-  b = new double[i]; 
-  c = new double[i]; 
+int main()
+  {
+   mat A = randu<mat>(5,5);
+   vec b = randu<vec>(5);
 
-  clock_t start, finish;
-  start = clock();
-  for (int j = 0; j < i; j++) {
-    a[j] = cos(j*1.0);
-    b[j] = sin(j+3.0);
-    c[j] = 0.0;
+  A.print("A =");
+  b.print("b=");
+  // solve Ax = b
+  vec x = solve(A,b);
+  // print x
+  x.print("x=");
+  // find LU decomp of A, if needed, P is the permutation matrix
+  mat L, U;
+  lu(L,U,A);
+  // print l
+  L.print(" L= ");
+  // print U
+  U.print(" U= ");
+  //Check that A = LU
+  (A-L*U).print("Test of LU decomposition");
+    return 0;
   }
-  for (int j = 0; j < i; j++) {
-    c[j] = a[j]+b[j];
-  }
-  finish = clock();
-  double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
-  cout << setiosflags(ios::showpoint | ios::uppercase);
-  cout << setprecision(10) << setw(20) << "Time used  for vector addition=" << timeused  << endl;
-  delete [] a;
-  delete [] b;
-  delete [] c;
-  return 0;           /* success execution of the program */
-}
