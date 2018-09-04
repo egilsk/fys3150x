09c7f09bce970f81c3c0c4e187b3ff400d006a0c
Egil Sperstad K|ller <egilsk@gaztak.uio.no>
2018-09-04 14:33:07 +0200

    Minor changes

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -19,22 +19,20 @@ inline double exact(double x) {return 1.0-(1-exp(-10))*x-exp(-10*x);}
 // Begin main program
 int main(int argc, char *argv[]){
   int exponent; 
-    string filename;
-    // We read also the basic name for the output file and the highest power of 10^n we want
+    // We read the highest power of 10^n we want
     if( argc <= 1 ){
           cout << "Bad Usage: " << argv[0] <<
-              " read also file name on same line and max power 10^n" << endl;
+              " reads max power 10^n" << endl;
           exit(1);
     }
         else{
-        filename = argv[1]; // first command line argument after name of program
-        exponent = atoi(argv[2]);
+        exponent = atoi(argv[1]);
     }
     // Loop over powers of 10
     for (int i = 1; i <= exponent; i++){
       int  n = (int) pow(10.0,i);
       // Declare new file name
-      string fileout = filename;
+      string fileout = "data";
       // Convert the power 10^i to a string
       string argument = to_string(i);
       // Final filename as filename-i-
@@ -42,7 +40,9 @@ int main(int argc, char *argv[]){
       fileout.append(".dat");
       double h = 1.0/(n+1);
       double hh = h*h;
-      // Set up arrays for the simple case
+
+
+      // Set up arrays for the general case
       vec a(n);  vec b(n);  vec c(n);  vec b_tilde(n);  vec x(n+2);  vec v(n+2);
       // Quick setup of updated diagonal elements and enpoint values of x and b
       v(0) = 0; v(n+1) = 0;
@@ -64,6 +64,12 @@ int main(int argc, char *argv[]){
 	for (int i = n-1; i > 0; i--){
 	  v(i) = (b_tilde(i-1) - c(i-1)*v(i+1))/b(i-1);
       }
+
+      
+      //Set up arrays for the special case
+	vec b_tilde_special(n); vec v_special(n)
+	
+				  
       // Now open file and write out results
       ofile.open(fileout);
       ofile << setiosflags(ios::showpoint | ios::uppercase);
