256323ba895ac2220431e09b4418214b0ecc9628
Egil Sperstad K|ller <egilsk@gaztak.uio.no>
2018-09-06 14:02:57 +0200

    Added special case and LU

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -5,32 +5,31 @@
 #include <string>
 #include <armadillo>
 #include <cstdlib>
+#include "time.h"
 
-// use namespace for output and input
 using namespace std;
 using namespace arma;
-// object for output files
+// Creating object for the output file
 ofstream ofile;
-// Functions used
+// Defining functions
 inline double f(double x) {return 100.0*exp(-10.0*x);}
 inline double exact(double x) {return 1.0-(1-exp(-10))*x-exp(-10*x);}
 
 
 // Begin main program
 int main(int argc, char *argv[]){
-  int exponent; 
-    // We read the highest power of 10^n we want
+  int exponent;
+    // We read the highest power of 10
     if( argc <= 1 ){
-          cout << "Bad Usage: " << argv[0] <<
-              " reads max power 10^n" << endl;
-          exit(1);
+      cout << "Bad Usage: " << argv[0] <<
+	" reads max power 10^n" << endl;
+      exit(1);
     }
-        else{
-        exponent = atoi(argv[1]);
+    else{
+      exponent = atoi(argv[1]);
     }
     // Loop over powers of 10
     for (int i = 1; i <= exponent; i++){
-      int  n = (int) pow(10.0,i);
       // Declare new file name
       string fileout = "data";
       // Convert the power 10^i to a string
@@ -38,9 +37,12 @@ int main(int argc, char *argv[]){
       // Final filename as filename-i-
       fileout.append(argument);
       fileout.append(".dat");
+      // Initialize the number og steps n and the step size h
+      int  n = (int) pow(10.0,i);
       double h = 1.0/(n+1);
       double hh = h*h;
 
+      // GENERAL CASE
 
       // Set up arrays for the general case
       vec a(n);  vec b(n);  vec c(n);  vec b_tilde(n);  vec x(n+2);  vec v(n+2);
@@ -53,6 +55,10 @@ int main(int argc, char *argv[]){
 	c(i) = -1.0;
 	b_tilde(i) = hh*f(x(i+1));
       }
+
+      // Running and timing the general algorithm
+      clock_t start, finish;
+      start = clock();
       // Forward substitution
       for (int i = 1; i < n; i++){
         double a_b = a(i-1)/b(i-1);
@@ -64,22 +70,82 @@ int main(int argc, char *argv[]){
 	for (int i = n-1; i > 0; i--){
 	  v(i) = (b_tilde(i-1) - c(i-1)*v(i+1))/b(i-1);
       }
-
+      finish = clock();
+      double time_general = (double) (finish - start)/(CLOCKS_PER_SEC);
       
-      //Set up arrays for the special case
-	vec b_tilde_special(n); vec v_special(n)
-	
-				  
+      // SPECIAL CASE
+
+      // Set up arrays for the special case
+      vec b_tilde_special(n); vec v_special(n+2);
+      // Setup of b-tilde and the endpoints of the solution vector
+      v_special(0) = 0; v_special(n+1) = 0;
+      for (int i = 0; i < n; i++){
+	b_tilde_special(i) = hh*f(x(i+1));
+      }
+
+      // Running and timing the special algorithm
+      start = clock();
+      // Forward substitution
+      for (int i = 1; i < n; i++) {
+	b_tilde_special(i) += i*b_tilde_special(i-1)/(i+1);
+      }			  
+      // Backward substitution
+      v_special(n) = n*b_tilde_special(n-1)/(n+1);
+      for (int i = n-1; i > 0; i--){
+	v_special(i) = i*(b_tilde_special(i-1) + v_special(i+1))/(i+1);
+      }
+      finish = clock();
+      double time_special = (double) (finish - start)/(CLOCKS_PER_SEC);
+
+      // LU-DECOMPOSITION
+      /*
+      // Initalize matrix and arrays
+      mat A = zeros<mat>(n,n);  vec b_tilde_lu(n);
+      // Set up matrix and arrays
+      A(n-1,n-1) = 2.0; 
+      for (int i = 0; i < n-1; i++){
+	b_tilde_lu(i) = hh*f(x(i+1));
+        A(i+1,i)  = -1.0;
+        A(i,i)    = 2.0;
+        A(i,i+1)  = -1.0;
+      }
+      // Solve Ax = b
+      vec v_lu = solve(A,b_tilde_lu);
+      */
+      mat A = zeros<mat>(n,n);
+      // Set up arrays for the simple case
+      vec b_lu(n);  vec x_lu(n);
+      A(0,0) = 2.0;  A(0,1) = -1;  x_lu(0) = h;  b_lu(0) =  hh*f(x_lu(0)); 
+      x_lu(n-1) = x_lu(0)+(n-1)*h; b_lu(n-1) = hh*f(x_lu(n-1)); 
+      for (int i = 1; i < n-1; i++){ 
+        x_lu(i) = x_lu(i-1)+h; 
+	b_lu(i) = hh*f(x_lu(i));
+        A(i,i-1)  = -1.0;
+        A(i,i)    = 2.0;
+        A(i,i+1)  = -1.0;
+      }
+      A(n-1,n-1) = 2.0; A(n-2,n-1) = -1.0; A(n-1,n-2) = -1.0;
+      // solve Ax = b
+      vec solution = solve(A,b_lu);
+
       // Now open file and write out results
       ofile.open(fileout);
       ofile << setiosflags(ios::showpoint | ios::uppercase);
       ofile << "       x:          approx:          exact:          "<< endl;
       for (int i = 0; i <= n+1 ;i++) {
+	double epsilon = log10(fabs((v(i) - exact(x(i)))/exact(x(i))));
 	ofile << setw(15) << setprecision(8) << x(i);
-        ofile << setw(15) << setprecision(8) << v(i);
-	ofile << setw(15) << setprecision(8) << exact(x(i)) << endl;
+        //ofile << setw(15) << setprecision(8) << v_lu(i);
+	ofile << setw(15) << setprecision(8) << exact(x(i));
+	ofile << setw(15) << setprecision(8) << epsilon << endl;
       }
+      ofile << "log10 of Step size:" << endl;
+      ofile << setw(15) << setprecision(8) << log10(h) << endl;
       ofile.close();
+      // Printing the timing-results
+      printf("Time used by general algorithm: %.4e\n", time_general);
+      printf("Time used by special algorithm: %.4e\n", time_special);
+
     }
     return 0;
 }
--- project1/project1.py
+++ project1/project1.py
@@ -3,15 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = int(sys.argv[1])
+h = np.zeros(N)
+max_error = np.zeros(N)
 
 for I in range(1, N+1):
     n = 10**I + 2
-    x = np.zeros((n,3))
+    x = np.zeros((n,4))
     datafile = "data{}.dat".format(I)
     with open(datafile, 'r') as infile:
         infile.readline()
         for i in range(n):
             x[i] = infile.readline().split()
+        infile.readline()
+        h[I-1] = infile.readline()
+    max_error[I-1] = max(x[1:-1,3])
     plt.figure(I)
     plt.plot(x[:,0],x[:,1],x[:,0],x[:,2])
     plt.savefig("plot{}.png".format(I))
+
+plt.figure(N+1)
+plt.plot(h,max_error)
+plt.savefig("plot_error.png")
