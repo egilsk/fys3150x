2f60e4deafdc8ec38f224114466782ae49f84508
Egil Sperstad K|ller <egilsk@gaztak.uio.no>
2018-09-08 17:30:48 +0200

    Finalize project 1

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -9,130 +9,147 @@
 
 using namespace std;
 using namespace arma;
-// Creating object for the output file
+// Create an object for the output data file
 ofstream ofile;
-// Defining functions
+// Define functions for the source term and the closed-form solution
 inline double f(double x) {return 100.0*exp(-10.0*x);}
-inline double exact(double x) {return 1.0-(1-exp(-10))*x-exp(-10*x);}
+inline double exact(double x) {return 1.0 - (1.0-exp(-10.0))*x - exp(-10.0*x);}
 
 
-// Begin main program
 int main(int argc, char *argv[]){
+  // Define a variable which stores the highest power of 10
   int exponent;
-    // We read the highest power of 10
+    // Read the highest power of 10 from the command line
     if( argc <= 1 ){
-      cout << "Bad Usage: " << argv[0] <<
-	" reads max power 10^n" << endl;
+      cout << "Error: " << argv[0] << " reads max power of 10" << endl;
       exit(1);
     }
     else{
       exponent = atoi(argv[1]);
     }
-    // Loop over powers of 10
+    // Loop over the powers of 10
     for (int i = 1; i <= exponent; i++){
-      // Declare new file name
+      // Give output data file the name data-i-.dat
       string fileout = "data";
-      // Convert the power 10^i to a string
       string argument = to_string(i);
-      // Final filename as filename-i-
       fileout.append(argument);
       fileout.append(".dat");
-      // Initialize the number og steps n and the step size h
-      int  n = (int) pow(10.0,i);
+
+      // Define the number of grid points (excluding start and end points) and the step size
+      int n = (int) pow(10.0,i);
       double h = 1.0/(n+1);
       double hh = h*h;
 
-      // GENERAL CASE
-
-      // Set up arrays for the general case
-      vec a(n);  vec b(n);  vec c(n);  vec b_tilde(n);  vec x(n+2);  vec v(n+2);
-      // Quick setup of updated diagonal elements and enpoint values of x and b
-      v(0) = 0; v(n+1) = 0;
+      // Define and set up an array for the x-values
+      vec x(n+2);
       for (int i = 0; i <= n+1; i++) x(i) = i*h; 
-      for (int i = 0; i < n; i++){ 
-	a(i) = -1.0;
-	b(i) = 2.0;
-	c(i) = -1.0;
-	b_tilde(i) = hh*f(x(i+1));
+
+      // GENERAL ALGORITHM ----------------------------------------------------------------------------------------------
+
+      // Define arrays
+      vec a_general(n); vec b_general(n); vec c_general(n); vec b_tilde_general(n); vec v_general(n+2);
+      // Set up the endpoints of v
+      v_general(0) = 0; v_general(n+1) = 0;
+      // Setup of the decomposed matrix
+      for (int i = 0; i <= n-1; i++){ 
+	a_general(i) = -1.0;
+	b_general(i) = 2.0;
+	c_general(i) = -1.0;
+	b_tilde_general(i) = hh*f(x(i+1));
       }
 
-      // Running and timing the general algorithm
+      // Run and time
       clock_t start, finish;
       start = clock();
       // Forward substitution
-      for (int i = 1; i < n; i++){
-        double a_b = a(i-1)/b(i-1);
-	b(i) -= a_b*c(i-1);
-	b_tilde(i) -= a_b*b_tilde(i-1);
+      for (int i = 1; i <= n-1; i++){
+        double a_b = a_general(i-1)/b_general(i-1);
+	b_general(i) -= a_b*c_general(i-1);
+	b_tilde_general(i) -= a_b*b_tilde_general(i-1);
       }
       // Backward substitution
-	v(n) = b_tilde(n-1)/b(n-1);
-	for (int i = n-1; i > 0; i--){
-	  v(i) = (b_tilde(i-1) - c(i-1)*v(i+1))/b(i-1);
+	v_general(n) = b_tilde_general(n-1)/b_general(n-1);
+	for (int i = n-1; i >= 1; i--){
+	  v_general(i) = (b_tilde_general(i-1) - c_general(i-1)*v_general(i+1))/b_general(i-1);
       }
       finish = clock();
       double time_general = (double) (finish - start)/(CLOCKS_PER_SEC);
       
-      // SPECIAL CASE
+      // SPECIAL ALGORITHM ---------------------------------------------------------------------------------------------
 
-      // Set up arrays for the special case
-      vec b_tilde_special(n); vec v_special(n+2);
-      // Setup of b-tilde and the endpoints of the solution vector
+      // Define arrays
+      vec b_special(n); vec b_tilde_special(n); vec v_special(n+2);
+      // Set up the endpoints of v
       v_special(0) = 0; v_special(n+1) = 0;
-      for (int i = 0; i < n; i++){
+      // Setup of the vectors b and b_tilde
+      for (int i = 0; i <= n-1; i++){
+	b_special(i) = (double) (i+2)/(i+1); 
 	b_tilde_special(i) = hh*f(x(i+1));
       }
 
-      // Running and timing the special algorithm
+      // Run and time
       start = clock();
       // Forward substitution
-      for (int i = 1; i < n; i++) {
-	b_tilde_special(i) += i*b_tilde_special(i-1)/(i+1);
-      }			  
+      for (int i = 1; i <= n-1; i++) {
+	b_tilde_special(i) += b_tilde_special(i-1)/b_special(i-1);
+      }
       // Backward substitution
-      v_special(n) = n*b_tilde_special(n-1)/(n+1);
-      for (int i = n-1; i > 0; i--){
-	v_special(i) = i*(b_tilde_special(i-1) + v_special(i+1))/(i+1);
+      v_special(n) = b_tilde_special(n-1)/b_special(n-1);
+      for (int i = n-1; i >= 1; i--){
+	v_special(i) = (b_tilde_special(i-1) + v_special(i+1))/b_special(i-1);
       }
       finish = clock();
       double time_special = (double) (finish - start)/(CLOCKS_PER_SEC);
 
-      // LU-DECOMPOSITION
-
-      // Initalize matrix and arrays
-      mat A = zeros<mat>(n,n);  vec b_tilde_lu(n);
-      // Set up matrix and arrays
+      // LU-DECOMPOSITION -----------------------------------------------------------------------------------------------
+      /*
+      // Define matrix and arrays
+      mat A = zeros<mat>(n,n);  vec b_tilde_lu(n); vec v_lu(n+2); 
+      // Set up the endpoints of v
+      v_lu(0) = 0; v_lu(n+1) = 0;
+      // Setup of the matrix and b_tilde
       A(n-1,n-1) = 2.0; b_tilde_lu(n-1) = hh*f(x(n));
-      for (int i = 0; i < n-1; i++){
-	b_tilde_lu(i) = hh*f(x(i+1));
+      for (int i = 0; i <= n-2; i++){
         A(i,i+1)  = -1.0;
         A(i,i)    = 2.0;
         A(i+1,i)  = -1.0;
+	b_tilde_lu(i) = hh*f(x(i+1));
       }
-      // Solve Ax = b
+
+      // Run and time
       start = clock();
-      vec v_lu = solve(A,b_tilde_lu);
+      // Solve Av = b
+      v_lu(span(1,n)) = solve(A,b_tilde_lu);
       finish = clock();
       double time_lu = (double) (finish - start)/(CLOCKS_PER_SEC);
+      */
+      //------------------------------------------------------------------------------------------------------------------
 
-      // Now open file and write out results
+      // Open output data file and write out results
       ofile.open(fileout);
       ofile << setiosflags(ios::showpoint | ios::uppercase);
-      ofile << "       x:          approx:          exact:          "<< endl;
+      ofile << "      x:             approx:        exact:         error:          "<< endl;
       for (int i = 0; i <= n+1 ;i++) {
-	double epsilon = log10(fabs((v(i) - exact(x(i)))/exact(x(i))));
+	// Compute the relative error
+	double epsilon = log10(fabs((v_special(i) - exact(x(i)))/exact(x(i))));
 	ofile << setw(15) << setprecision(8) << x(i);
-        ofile << setw(15) << setprecision(8) << v(i);
+        ofile << setw(15) << setprecision(8) << v_special(i);
 	ofile << setw(15) << setprecision(8) << exact(x(i));
 	ofile << setw(15) << setprecision(8) << epsilon << endl;
       }
-      ofile << "log10 of Step size:" << endl;
+      
+      // Write out log10 of the step size
+      ofile << "log10(h):" << endl;
       ofile << setw(15) << setprecision(8) << log10(h) << endl;
       ofile.close();
-      // Printing the timing-results
+
+      // Print the timing-results
+      /*
+      printf("n = 10^%d\n", i);
       printf("Time used by general algorithm: %.4f\n", time_general);
       printf("Time used by special algorithm: %.4f\n", time_special);
       printf("Time used by LU-decomposition algorithm: %.4f\n", time_lu);
+      */
     }
     return 0;
 }
