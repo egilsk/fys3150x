01afe13f6a049450339e2754a1e795bfec966199
Egil Sperstad K|ller <egilsk@gaztak.uio.no>
2018-10-11 19:28:32 +0200

    Minor changes

--- project3/project3.py
+++ project3/project3.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import re
 
 # Read the name of the input data file from the command line
@@ -20,7 +21,37 @@ with open(datafile + ".dat", 'r') as infile:
     infile.readline()
     for i in range(n):
         data[i] = infile.readline().split()
-    
+
+# Interactive mode on
+plt.ion()
+
+# Set up a figure
+fig = plt.figure()
+fig.suptitle("Solar system")
+ax = fig.add_subplot(111)
+ax.grid('on')
+ax.set_xlabel("x [AU]")
+ax.set_ylabel("y [AU]")
+ax.axis("equal")
+ax.axis([-2 , 2, -2, 2])
+
+# Plot the inital system
+line, = ax.plot(data[0,1], data[0,2], 'o')
+
+plt.draw()
+
+# Animation
+for i in range(1,n):
+    line.set_xdata(data[i,1])
+    line.set_ydata(data[i,2])
+    plt.draw()
+    plt.pause(0.01)
+
+# Interactive mode off
+plt.ioff()
+
+
+
 # Plot the results
 plt.plot(data[:,1], data[:,2], label = datafile.split("_")[-1])
 plt.xlabel("x")
